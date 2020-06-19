8b12c8b7bcc240d7f07504bd6ea1d06cd9da6b29
mrjoshc <hello@joshcummingsdesign.com>
2020-06-18 17:59:21 -0700

    Update ses config

--- src/scripts/ses.js
+++ src/scripts/ses.js
@@ -1,5 +1,11 @@
 const AWS = require('aws-sdk');
 
-AWS.config.update({ region: process.env.AWS_SES_REGION });
+AWS.config.update({
+  credentials: new AWS.Credentials({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  }),
+  region: process.env.AWS_SES_REGION,
+});
 
 module.exports = new AWS.SES();
