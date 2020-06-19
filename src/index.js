beb0bc2690fa0d8bdd3bdf5ad17d4b753f7df325
joshcummingsdesign <joshcummingsdesign@gmail.com>
2020-06-18 17:32:55 -0700

    Add create command

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const cli = require('yargs');
 const serve = require('./scripts/start');
+const create = require('./scripts/create');
 const push = require('./scripts/push');
 const pull = require('./scripts/pull');
 
@@ -10,6 +11,7 @@ cli
   .scriptName('ses')
   .version('v1.0.0')
   .command('start', 'start the local development server', getTemplateName, serve)
+  .command('create [name]', 'create a new template', getTemplateName, create)
   .command('push [name]', 'push a template to SES', getTemplateName, push)
   .command('pull [name]', 'pull a template from SES', getTemplateName, pull)
   .strict().argv;
