40a2edc406acc26760ef2b19be5788846f259d7c
mrjoshc <hello@joshcummingsdesign.com>
2020-06-18 17:41:39 -0700

    Fix live-server

--- src/scripts/start.js
+++ src/scripts/start.js
@@ -1,10 +1,8 @@
 const liveServer = require('live-server');
 
-module.exports = () => {
-  liveServer.start({
-    port: process.env.PORT,
-    host: 'localhost',
-    root: 'src/public',
-    open: true,
-  });
-};
+liveServer.start({
+  port: process.env.PORT,
+  host: 'localhost',
+  root: 'src/public',
+  open: true,
+});
