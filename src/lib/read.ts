0543f61b7a7e0226e52efa71bb4a05773cd9a771
joshcummingsdesign <joshcummingsdesign@gmail.com>
2020-06-20 14:19:50 -0700

    Fix paths by using join

--- src/lib/read.ts
+++ src/lib/read.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import path from 'path';
 import util from 'util';
 import chalk from 'chalk';
 import { SES } from 'aws-sdk';
@@ -7,12 +8,15 @@ import { exitWithCode, ErrorCode } from '../utils/error';
 
 export const read = async (name: string): Promise<SES.CreateTemplateRequest> => {
   const readFile = util.promisify(fs.readFile);
-  const dir = `${PUBLIC_DIR}/${name}`;
+  const dir = path.join(PUBLIC_DIR, name);
+  const htmlFile = path.join(dir, 'index.html');
+  const textFile = path.join(dir, 'index.txt');
+  const templateFile = path.join(dir, 'template.json');
 
   console.log(chalk.gray(`${name}: Reading template files...`));
-  const HtmlPart = await readFile(`${dir}/index.html`, 'utf8').catch(exitWithCode(ErrorCode.IO));
-  const TextPart = await readFile(`${dir}/index.txt`, 'utf8').catch(exitWithCode(ErrorCode.IO));
-  const SubjectPart = await readFile(`${dir}/template.json`, 'utf8')
+  const HtmlPart = await readFile(htmlFile, 'utf8').catch(exitWithCode(ErrorCode.IO));
+  const TextPart = await readFile(textFile, 'utf8').catch(exitWithCode(ErrorCode.IO));
+  const SubjectPart = await readFile(templateFile, 'utf8')
     .then((res) => JSON.parse(res).subject as string)
     .catch(exitWithCode(ErrorCode.IO));
 
