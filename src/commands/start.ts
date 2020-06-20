bdd8618d7d7de6c01c85e792755a7be2a0e57c5f
joshcummingsdesign <joshcummingsdesign@gmail.com>
2020-06-20 11:09:46 -0700

    Update readme

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,6 +1,7 @@
 import { Command } from 'commander';
 import liveServer from 'live-server';
 import config from '../config';
+import { spawnPublic } from '../lib/spawnPublic';
 
 /**
  * Start the local development server.
@@ -9,11 +10,13 @@ export const start = (cli: Command) =>
   cli
     .command('start')
     .description('start the local development server')
-    .action(() =>
+    .action(async () => {
+      await spawnPublic();
+
       liveServer.start({
         port: Number(config.port),
         host: 'localhost',
         root: 'public',
         open: true,
-      })
-    );
+      });
+    });
