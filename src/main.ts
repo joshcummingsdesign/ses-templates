96a30a60a0ef5d6e17b3f2703eb38e0f622d2861
mrjoshc <hello@joshcummingsdesign.com>
2020-06-22 23:58:46 -0700

    Add integration tests

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
+import { Command } from 'commander';
 import { CliFactory } from './cli';
-import { SesService } from './services/ses.service';
+import sesService from './services/ses.service';
 
 function bootstrap() {
-  const cli = new CliFactory(new SesService());
-  cli.init();
+  const cli = new CliFactory(new Command() as Command, sesService);
+  cli.run();
 }
 bootstrap();
